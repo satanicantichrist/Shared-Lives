b42489a69fe13d463975827c9a475e59b8a6b4dc
satanicantichrist <burianek.jan@protonmail.com>
2025-09-08 21:42:35 +0200

    fixed some imports

--- src/main/java/me/satanicantichrist/sharedlives/Sharedlives.java
+++ src/main/java/me/satanicantichrist/sharedlives/Sharedlives.java
@@ -1,21 +1,20 @@
 package me.satanicantichrist.sharedlives;
 
-import me.satanicantichrist.sharedlives.events.PlayerDamageEvents;
+import me.satanicantichrist.sharedlives.events.PlayerEvents;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Level;
-
 public final class Sharedlives extends JavaPlugin {
+
     public static Plugin plugin;
+
     @Override
     public void onEnable() {
         saveResource("config.yml", false);
         plugin = this;
-        this.getServer().getPluginManager().registerEvents(new PlayerDamageEvents(), this);
+        this.getServer().getPluginManager().registerEvents(new PlayerEvents(), this);
 
         getLogger().info("Options: ");
         getLogger().info("Share heal: " + getConfig().getBoolean("share-heal"));
     }
-
 }
